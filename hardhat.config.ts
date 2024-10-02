80cb4590d15a3c4c9a5677fe917cf4647953319d
avinashkt04 <kumaravinash12915@gmail.com>
2024-10-02 14:03:29 +0530

    improving hardhat configuration

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,10 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "hardhat-deploy";
 
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || ("" as string);
+const PRIVATE_KEY = process.env.PRIVATE_KEY || ("" as string);
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ("" as string);
+
 const config: HardhatUserConfig = {
   solidity: "0.8.27",
   defaultNetwork: "hardhat",
@@ -13,14 +17,21 @@ const config: HardhatUserConfig = {
     localhost: {
       chainId: 31337,
       allowUnlimitedContractSize: true,
-    }
+    },
+    sepolia: {
+      chainId: 11155111,
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
   },
   namedAccounts: {
     deployer: {
       default: 0,
-    }
+    },
   },
-  
 };
 
 export default config;
